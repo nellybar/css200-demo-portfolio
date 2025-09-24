51b49374300664cd3d958e34e658260750101cb1
Nelly Barrera <117851716+nellybar@users.noreply.github.com>
2025-09-24 17:01:30 -0600

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <nav>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Experience</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright &#169; {year} Nelly Barrera. All Rights Reserved.</p>
+    </footer>
+  );
+}
